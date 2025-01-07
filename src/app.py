67d8fe4f33889d22b68ef7d28fe9878d34359d43
Abid Khawarizmy <abidalkhaw@gmail.com>
2025-01-07 16:49:20 +0800

    fixing pop() func

--- src/app.py
+++ src/app.py
@@ -16,9 +16,18 @@ english_stops = set(stopwords.words('english'))
 MODEL_PATH = "./models/LSTM.h5.keras"
 TOKENIZER_PATH = "./models/tokenizer.pkl"
 
-model = load_model(MODEL_PATH)
-tokenizer = joblib.load(TOKENIZER_PATH)
-MAX_LENGTH = 27  # Harus sama dengan max_length saat training
+# Pengecekan model dan tokenizers
+try:
+    model = load_model(MODEL_PATH, compile=False)
+except Exception as e:
+    st.error(f"Terjadi kesalahan saat memuat model: {str(e)}")
+
+try:
+    tokenizer = joblib.load(TOKENIZER_PATH)
+except Exception as e:
+    st.error(f"Terjadi kesalahan saat memuat tokenizer: {str(e)}")
+
+MAX_LENGTH = 17  # Harus sama dengan max_length saat training
 
 # Fungsi preprocessing
 def preprocess_text(text):
@@ -37,18 +46,25 @@ def predict_sentiment(text):
     preprocessed_text = preprocess_text(text)
     text_seq = tokenizer.texts_to_sequences([preprocessed_text])
     text_pad = pad_sequences(text_seq, maxlen=MAX_LENGTH, padding='post', truncating='post')
-    prediction = model.predict(text_pad)
-    sentiment = "Positive" if prediction > 0.5 else "Negative"
-    return sentiment, prediction[0][0]
+    
+    try:
+        prediction = model.predict(text_pad)
+        if prediction is None or prediction.size == 0:
+            st.error("Prediksi tidak valid.")
+            return "Invalid", 0.0
+        sentiment = "Positive" if prediction > 0.5 else "Negative"
+        return sentiment, prediction[0][0]
+    except Exception as e:
+        st.error(f"Terjadi kesalahan saat prediksi: {str(e)}")
+        return "Error", 0.0
 
 # Informasi aplikasi
 st.set_page_config(
-    page_title="Prediksi Sentimen Amazon Store", 
+    page_title="Amazon Prediction Sentiment", 
     page_icon="./assets/favicon.ico" 
 )
 
-st.title("Prediksi Sentimen Aplikasi Amazon Store")
-st.write("Aplikasi ini memprediksi sentimen ulasan dari pengguna menggunakan model LSTM.")
+st.title("Sentiment Analysis Amazon")
 
 # Input ulasan dari pengguna
 user_input = st.text_area("Masukkan ulasan aplikasi Anda di bawah ini:", "")
@@ -59,4 +75,3 @@ if submit and user_input.strip():
     sentiment, confidence = predict_sentiment(user_input)
     st.write(f"### Hasil Prediksi")
     st.write(f"**Sentimen**: {sentiment}")
-    st.write(f"**Confidence Score**: {confidence:.2f}")
